c8db5f13f3c681014fa21e5517e7a027aa8378db
amantaymagzum <92146836+amantaymagzum@users.noreply.github.com>
2021-10-08 13:05:12 +0600

    Амантай Магзум 211/3 комп.сайнс

---
+++ 1.py
@@ -0,0 +1,15 @@
+num1=int(input("Введите превое число:"))
+
+num2=int(input("Введите второе число:"))
+
+num1*=5
+print("Results:",num1+num2)
+print("Results:",num1-num2)
+print("Results:",num1/num2)
+print("Results:",num1*num2)
+print("Results:",num1**num2)
+print("Results:",num1//num2)
+
+word="Hi"
+print(word*2)
+word=True
---
+++ 5.py
@@ -0,0 +1,13 @@
+count=0
+word="Hello World"
+for i in  word:
+    if i == "w":
+        count +=1
+    print("count",count)
+
+isHasCar=True
+
+while isHasCar:
+            if input("Enter data")=="Stop":
+                isHasCar=False
+
